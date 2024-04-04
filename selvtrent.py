d3ed7ce372eda55735572364236011d0702d1bd6
mariu <mariusbj@outlook.com>
2024-04-04 13:06:59 +0200

    trening av model på pascal voc 2007

--- selvtrent.py
+++ selvtrent.py
@@ -119,7 +119,89 @@ augmenter_fn = create_augmenter_fn(augmenters)
 
 train_ds = train_ds.map(augmenter_fn, num_parallel_calls=tf_data.AUTOTUNE)
 visualize_dataset(
-    train_ds, bounding_box_format="xywh", value_range=(0, 255), rows=2, cols=2
+    train_ds, bounding_box_format="xywh", value_range=(0, 255), rows=2, cols=2, path="augmented.png"
+)
+
+inference_resizing = keras_cv.layers.Resizing(
+    640, 640, bounding_box_format="xywh", pad_to_aspect_ratio=True
+)
+eval_ds = eval_ds.map(inference_resizing, num_parallel_calls=tf_data.AUTOTUNE)
+
+visualize_dataset(
+    eval_ds, bounding_box_format="xywh", value_range=(0, 255), rows=2, cols=2, path="inference.png"
+)
+
+def dict_to_tuple(inputs):
+    return inputs["images"], bounding_box.to_dense(
+        inputs["bounding_boxes"], max_boxes=32
+    )
+
+
+train_ds = train_ds.map(dict_to_tuple, num_parallel_calls=tf_data.AUTOTUNE)
+eval_ds = eval_ds.map(dict_to_tuple, num_parallel_calls=tf_data.AUTOTUNE)
+
+train_ds = train_ds.prefetch(tf_data.AUTOTUNE)
+eval_ds = eval_ds.prefetch(tf_data.AUTOTUNE)
+
+base_lr = 0.005
+# including a global_clipnorm is extremely important in object detection tasks
+optimizer = keras.optimizers.SGD(
+    learning_rate=base_lr, momentum=0.9, global_clipnorm=10.0
 )
 
 
+coco_metrics_callback = keras_cv.callbacks.PyCOCOCallback(
+    eval_ds, bounding_box_format="xywh"
+)
+
+model = keras_cv.models.YOLOV8Detector.from_preset(
+    "resnet50_imagenet",
+    # For more info on supported bounding box formats, visit
+    # https://keras.io/api/keras_cv/bounding_box/
+    bounding_box_format="xywh",
+    num_classes=20,
+)
+
+model.compile(
+    classification_loss="binary_crossentropy",
+    box_loss="ciou",
+    optimizer=optimizer,
+)
+
+model.fit(
+    train_ds.take(20),
+    # Run for 10-35~ epochs to achieve good scores.
+    epochs=10,
+    callbacks=[coco_metrics_callback],
+)
+
+visualization_ds = eval_ds.unbatch()
+visualization_ds = visualization_ds.ragged_batch(16)
+visualization_ds = visualization_ds.shuffle(8)
+
+def visualize_detections(model, dataset, bounding_box_format,path):
+    images, y_true = next(iter(dataset.take(1)))
+    y_pred = model.predict(images)
+    visualization.plot_bounding_box_gallery(
+        images,
+        value_range=(0, 255),
+        bounding_box_format=bounding_box_format,
+        y_true=y_true,
+        y_pred=y_pred,
+        scale=4,
+        rows=2,
+        cols=2,
+        show=True,
+        font_scale=0.7,
+        class_mapping=class_mapping,
+        path=path
+    )
+
+model.prediction_decoder = keras_cv.layers.NonMaxSuppression(
+    bounding_box_format="xywh",
+    from_logits=True,
+    iou_threshold=0.5,
+    confidence_threshold=0.75,
+)
+
+visualize_detections(model, dataset=visualization_ds, bounding_box_format="xywh",path='detections.png')
