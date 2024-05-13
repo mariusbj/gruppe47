10124973454f7a22cb019f7f9ce93769d224797f
mariu <mariusbj@outlook.com>
2024-05-13 21:51:24 +0200

    la til konvertering for map

--- selvtrent.py
+++ selvtrent.py
@@ -74,7 +74,6 @@ def load_pascal_voc(split, dataset, bounding_box_format):
     )
     
     return ds
-
 train_ds = load_pascal_voc(
     split="train", dataset="voc/2007", bounding_box_format="xywh"
 )
@@ -84,19 +83,6 @@ train_ds = train_ds.shuffle(BATCH_SIZE * 4)
 
 train_ds = train_ds.ragged_batch(BATCH_SIZE, drop_remainder=True)
 eval_ds = eval_ds.ragged_batch(BATCH_SIZE, drop_remainder=True)
-
-visualize_dataset(
-    train_ds, bounding_box_format="xywh", value_range=(0, 255), rows=2, cols=2,path="train.png",
-)
-
-visualize_dataset(
-    eval_ds,
-    bounding_box_format="xywh",
-    value_range=(0, 255),
-    rows=2,
-    cols=2,
-    path="eval.png",
-)
 
 augmenters = [
     keras_cv.layers.RandomFlip(mode="horizontal", bounding_box_format="xywh"),
@@ -116,11 +102,6 @@ def create_augmenter_fn(augmenters):
 
 
 augmenter_fn = create_augmenter_fn(augmenters)
-
-train_ds = train_ds.map(augmenter_fn, num_parallel_calls=tf_data.AUTOTUNE)
-visualize_dataset(
-    train_ds, bounding_box_format="xywh", value_range=(0, 255), rows=2, cols=2, path="augmented.png"
-)
 
 inference_resizing = keras_cv.layers.Resizing(
     640, 640, bounding_box_format="xywh", pad_to_aspect_ratio=True
@@ -144,64 +125,33 @@ train_ds = train_ds.prefetch(tf_data.AUTOTUNE)
 eval_ds = eval_ds.prefetch(tf_data.AUTOTUNE)
 
 base_lr = 0.005
-# including a global_clipnorm is extremely important in object detection tasks
+
 optimizer = keras.optimizers.SGD(
     learning_rate=base_lr, momentum=0.9, global_clipnorm=10.0
 )
-
 
 coco_metrics_callback = keras_cv.callbacks.PyCOCOCallback(
     eval_ds.take(20), bounding_box_format="xywh"
 )
 
 model = keras_cv.models.YOLOV8Detector.from_preset(
-    "resnet50_imagenet",
-    # For more info on supported bounding box formats, visit
-    # https://keras.io/api/keras_cv/bounding_box/
+    "yolo_v8_m_pascalvoc",
     bounding_box_format="xywh",
     num_classes=20,
 )
 
-model.compile(
-    classification_loss="binary_crossentropy",
-    box_loss="ciou",
-    optimizer=optimizer,
-)
 
-model.fit(
-    train_ds.take(20),
-    # Run for 10-35~ epochs to achieve good scores.
-    epochs=10,
-    callbacks=[coco_metrics_callback],
-)
 
-visualization_ds = eval_ds.unbatch()
-visualization_ds = visualization_ds.ragged_batch(16)
-visualization_ds = visualization_ds.shuffle(8)
-
-def visualize_detections(model, dataset, bounding_box_format,path):
-    images, y_true = next(iter(dataset.take(1)))
-    y_pred = model.predict(images)
-    visualization.plot_bounding_box_gallery(
-        images,
-        value_range=(0, 255),
-        bounding_box_format=bounding_box_format,
-        y_true=y_true,
-        y_pred=y_pred,
-        scale=4,
-        rows=2,
-        cols=2,
-        show=False,
-        font_scale=0.7,
-        class_mapping=class_mapping,
-        path=path
-    )
-
-model.prediction_decoder = keras_cv.layers.NonMaxSuppression(
+retina_model = keras_cv.models.RetinaNet.from_preset(
+    "retinanet_resnet50_pascalvoc",
+    num_classes=20,
     bounding_box_format="xywh",
-    from_logits=True,
-    iou_threshold=0.5,
-    confidence_threshold=0.75,
 )
 
-visualize_detections(model, dataset=visualization_ds, bounding_box_format="xywh",path='detections.png')
+retina_model.compile(
+    optimizer=optimizer,
+    box_loss="SmoothL1",
+    metrics=["precision","recall"],
+)
+
+evaluation_result = retina_model.evaluate(eval_ds.take(10))
